28393b2845cf263f7e71755526033ed53032a47a
gmD3v <gussmotab@gmail.com>
2024-02-10 01:46:26 -0400

    feat(cmd): :sparkles: Add environment variable loading to main.go

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "gmd3v.com/sgotify/cmd"
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+	"gmd3v.com/sgotify/cmd"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	cmd.Execute()
 }

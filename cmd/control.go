5a584d8b3ef1a3ccf4a63c2ee44d774e47b7a003
gmD3v <gussmotab@gmail.com>
2024-02-10 01:47:51 -0400

    feat(cmd): :sparkles: Add control command and handlers
    Add basic commands like play, pause, next and prev song

--- cmd/control.go
+++ cmd/control.go
@@ -1,6 +1,5 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
@@ -8,33 +7,47 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"gmd3v.com/sgotify/internal/core/services/control"
+	controlhandler "gmd3v.com/sgotify/internal/handlers/control_handler"
+	"gmd3v.com/sgotify/internal/repositories/controlrepo"
+	"gmd3v.com/sgotify/internal/repositories/tokenrepo"
 )
 
-// controlCmd represents the control command
-var controlCmd = &cobra.Command{
-	Use:   "control",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+func init() {
+	tokenRepo := tokenrepo.NewTokenRepository()
+	controlRepo := controlrepo.NewWrpControl()
+	controlSrv := control.NewService(controlRepo, tokenRepo)
+	handler := controlhandler.NewHandler(controlSrv)
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "control",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("control called")
-	},
-}
+		Run: func(cmd *cobra.Command, args []string) {
+			// get command type from command and call the appropriate function
+			if len(args) == 0 {
+				fmt.Println("Please provide a command")
+				return
+			}
+			command := args[0]
+			if command == "play" {
+				handler.PlaySong(cmd, args)
+			}
+			if command == "pause" {
+				handler.PauseSong(cmd, args)
+			}
+			if command == "next" {
+				handler.NextSong(cmd, args)
+			}
+			if command == "previous" {
+				handler.PreviousSong(cmd, args)
+			}
 
-func init() {
-	rootCmd.AddCommand(controlCmd)
+		},
+	})
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// controlCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// controlCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }

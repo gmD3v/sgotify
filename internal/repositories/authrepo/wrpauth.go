d253bf498d8c7c9837685024250ec581abe123e9
gmD3v <gussmotab@gmail.com>
2024-02-11 01:32:01 -0400

    feat(repositories): :sparkles: Add IsTokenValid function to WRPAuth

--- internal/repositories/authrepo/wrpauth.go
+++ internal/repositories/authrepo/wrpauth.go
@@ -3,6 +3,7 @@ package authrepo
 import (
 	"context"
 
+	"github.com/zmb3/spotify/v2"
 	wrapperutil "gmd3v.com/sgotify/internal/utils/wrapper_util"
 	"golang.org/x/oauth2"
 )
@@ -33,9 +34,9 @@ func (w *WRPAuth) GetToken(code string) (string, string, error) {
 }
 
 // RefreshToken is a function that returns a token and a refresh token
-func (w *WRPAuth) RefreshToken(accessToken, refresh string) (string, string, error) {
+func (w *WRPAuth) RefreshToken(refresh string) (string, string, error) {
 	context := context.Background()
-	tok, err := utils.GetAuthenticator().RefreshToken(context, &oauth2.Token{RefreshToken: refresh, AccessToken: accessToken})
+	tok, err := utils.GetAuthenticator().RefreshToken(context, &oauth2.Token{RefreshToken: refresh, TokenType: "Bearer"})
 	if err != nil {
 		return "", "", err
 	}
@@ -45,4 +46,11 @@ func (w *WRPAuth) RefreshToken(accessToken, refresh string) (string, string, err
 // Get URL for authentication
 func (w *WRPAuth) GetAuthURL() string {
 	return utils.GetAuthenticator().AuthURL("abc123")
+}
+
+func (w *WRPAuth) IsTokenValid(accessToken string) bool {
+	ctx := context.Background()
+	client := spotify.New(utils.GetAuthenticator().Client(ctx, &oauth2.Token{AccessToken: accessToken, TokenType: "Bearer"}))
+	_, err := client.CurrentUser(ctx)
+	return err == nil
 }

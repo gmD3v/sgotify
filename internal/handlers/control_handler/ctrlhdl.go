91116c162f8eca24428779cb0592e71306c182e2
gmD3v <gussmotab@gmail.com>
2024-02-12 00:02:54 -0400

    feat(handlers): :sparkles: Add ControllerMain function to handle control commands

--- internal/handlers/control_handler/ctrlhdl.go
+++ internal/handlers/control_handler/ctrlhdl.go
@@ -15,6 +15,29 @@ func NewHandler(service ports.ControlService) *ControlHandler {
 	return &ControlHandler{ctrlService: service}
 }
 
+func (h *ControlHandler) ControllerMain(cmd *cobra.Command, args []string) {
+	var err error
+	var controllers = map[string]func(*cobra.Command, []string){
+		"play":     h.PlaySong,
+		"pause":    h.PauseSong,
+		"next":     h.NextSong,
+		"previous": h.PreviousSong,
+		"get":      h.GetSong,
+	}
+	if len(args) == 0 {
+		cmd.Help()
+		return
+	}
+	if controller, ok := controllers[args[0]]; ok {
+		controller(cmd, args)
+	} else {
+		err = fmt.Errorf("Invalid command %s", args[0])
+	}
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
 func (h *ControlHandler) PlaySong(cmd *cobra.Command, args []string) {
 	var err error
 	if len(args) >= 2 {
@@ -53,9 +76,9 @@ func (h *ControlHandler) PreviousSong(cmd *cobra.Command, args []string) {
 }
 
 func (h *ControlHandler) GetSong(cmd *cobra.Command, args []string) {
-	_, err := h.ctrlService.GetSong()
+	song, err := h.ctrlService.GetSong()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	fmt.Println("Getting song")
+	fmt.Printf("Song: %s\nArtist: %s\nAlbum: %s\nRelease Date: %s\nLength: %d\nIs Playing: %t\n", song.Name, song.Artist, song.Album, song.ReleaseDate, song.Length, song.IsPlaying)
 }
